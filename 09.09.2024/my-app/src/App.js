5c3ec790b15218598236eb2433c18bd8c38822a6
Sa1tama <159195533+Sa1tamaMan@users.noreply.github.com>
2024-09-15 18:01:46 +0300

    (15.09.2024) kodutoo 2. commit
    Homework 2nd exercise done.

--- 09.09.2024/my-app/src/App.js
+++ 09.09.2024/my-app/src/App.js
@@ -4,26 +4,31 @@ import Name from "./components/Name";
 import Counter from './components/Counter';
 
 function App() {
+  const user = {
+    name: 'Cat Paw',
+    imageUrl: 'https://lirp.cdn-website.com/1ce4dbecaee64753bdb182e3ac314b06/dms3rep/multi/opt/ThinkstockPhotos-149052633-1920w.jpg',
+    imageSize: 90,
+  };
+  
   return (
     <>
       <Name title="Kris" />
       <Name />
       <Counter />
+      <hr></hr>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
   )
 
-  /* {
-    "semi": false,
-    "tabWidth": 2,
-    "printWidth": 80,
-    "singleQuote": false,
-    "jsxSingleQuote": false,
-    "bracketSpacing": true,
-    "bracketSameLine": false,
-    "trailingComma": "all",
-    "arrowParens": "avoid",
-    "singleAttributePerLine": true
-  } */
 }
 
 export default App;
